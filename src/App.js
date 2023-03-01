d6e397ea5c7b518efc1463dc949b0edb738570a8
Hemal99 <h.githmin@gmail.com>
2023-03-01 16:44:17 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [userData, setUserData] = useState({
+    gender: "loading...",
+    age: "loading...",
+  });
+  const [img, setImage] = useState("");
+
+  const callBackEnd = async () => {
+    fetch("http://127.0.0.1:5000/detector")
+      .then((response) => response.json())
+      .then((data) => setUserData(data));
+  };
+
+  useEffect(() => {
+    callBackEnd();
+    setTimeout(() => {
+      setImage("http://localhost:5000/video_feed");
+    }, 3000);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "center",
+        alignContent: "center",
+        background: "yellow",
+      }}
+    >
+      <div style={{ width: "60%", padding: "5rem" }}>
+        <div>
+          <img src={img} alt="Video" />
+        </div>
+      </div>
+      <div style={{ width: "60%", padding: "5rem" }}>
+        <div>
+          <div>Gender : {userData.gender}</div>
+          <div>Age : {userData.age}</div>
+        </div>
+      </div>
     </div>
   );
 }
