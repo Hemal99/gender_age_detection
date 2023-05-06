b7ec020994f86a6b47b764b7526cd28e8ad34031
vinulmanjitha <vinulmanjitha98@gmail.com>
2023-05-06 22:27:26 +0530

    hihemas

--- src/App.js
+++ src/App.js
@@ -9,15 +9,29 @@ function App() {
   const [img, setImage] = useState("");
 
   const callBackEnd = async () => {
-    fetch("http://127.0.0.1:5000/detector")
+    fetch("http://127.0.0.1:5000/api/v1/hci/detector")
       .then((response) => response.json())
       .then((data) => setUserData(data));
+   
+  };
+  const save_user = async () => {
+    const body={gender:userData.gender,age:userData.age}
+    fetch('http://127.0.0.1:5000/api/v1/hci/user', {
+      Method: 'POST',
+      Headers: {
+        Accept: 'application.json',
+        'Content-Type': 'application/json'
+      },
+      Body: body,
+      Cache: 'default'
+    })
+   
   };
 
   useEffect(() => {
     callBackEnd();
     setTimeout(() => {
-      setImage("http://localhost:5000/video_feed");
+      setImage("http://localhost:5000/api/v1/hci/video_feed");
     }, 3000);
   }, []);
 
@@ -41,9 +55,16 @@ function App() {
           <div>Gender : {userData.gender}</div>
           <div>Age : {userData.age}</div>
         </div>
+        <div>
+          <button onClick={save_user}>
+
+            save_user
+            </button>
+        </div>
       </div>
     </div>
   );
 }
 
 export default App;
+
